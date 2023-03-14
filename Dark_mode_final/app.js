90c7f2fdc1a601d7a88b986711ddfc404e6e901f
abbas1551 <119072166+abbas1551@users.noreply.github.com>
2023-03-15 01:48:41 +0530

    Update app.js

--- Dark_mode_final/app.js
+++ Dark_mode_final/app.js
@@ -14,13 +14,13 @@ function Creat_Custom_Element(tag, attr_tag, attr_name, value){
 }
 
 
-if (site.includes("classroom.google.com") || site.includes("codeforces.com")){
+if (site.includes("classroom.google.com")){
     alert("This website: "+ site+" is not safe for your mental health")
     Add_Custom_style(`
     
     
     body {
-        background-color: #2A1342;
+        background-color: #252525;
         
       }
         
@@ -33,23 +33,52 @@ if (site.includes("classroom.google.com") || site.includes("codeforces.com")){
         background-color: black;/*top bar*/
     }
     .OX4Vcb{
-        background-color: pink;/*slider bar*/
+        background-color: #1C0036;/*slider bar*/
     }
     .YVvGBb{
         color : white;/*text1*/
     }
     .gHz6xd{
-        background-color: darkblue;/*bottom of each class tile*/
-        color: red;/*folder*/
+        background-color: #2C2F54;/*bottom of each class tile*/
+        color: lightgrey;/*folder*/
     }
     .PeGHgb{
-        background-color: green;/*comment*/
+        background-color: #3E3E3E;/*comment*/
     }
     .pco8Kc{
-        color:red;/*text inside box*/
+        color:lightgrey;/*text inside box*/
     }
     .oBSRLe{
         color:yellow;/**/
+    }
+    .nGi02b{
+        color:yellow;/*text when open assignment*/
+    }
+    .K6Ovqd {
+        color:pink;
+    }
+    .NPEfkd, .NMm5M{
+        /*background-color: red;*/
+        color:white; /*class comment under the post*/
+    }
+    .gJItbc{
+        color:yellow;/*name of commentator*/
+    }
+    .onkcGd.zZN2Lb-Wvd9Cc.VBEdtc-Wvd9Cc.apFsO {
+        color:white  ;/*home work deadline on home page*/
+        
+    }
+    .sjxkNc {
+        color: yellow;/*color of to do example this weel next week past week*/
+    }
+    .HbKQLd.WOPwXe{
+        color: white;/*same as above but the one not selected*/
+    }
+    .VSWCL.tLDEHd{
+        color: lightgrey;/* comments */
+    }
+    .bswVrf.Lzdwhd-BrZSOd{
+        color: grey;
     }
 
       
